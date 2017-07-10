911c1ed50f447d68a350c76c2d9bdd7f9142f9c0
estebandalelr <estebandalelr@gmail.com>
2017-07-10 17:56:56 -0500

    finished ex5

---
+++ exercises/ex5.py
@@ -0,0 +1,15 @@
+my_name = "Esteban"
+my_age = 22
+my_height = 186 #cms
+my_weight = 71 #Kgs
+my_eyes = "Black"
+my_theeth = "Almost white"
+my_hair = "Black"
+
+print "Let's talk about %s" %my_name
+print "He's %d cms tall" %my_height
+print "He weights %d Kgs" %my_weight
+print "He's got %s eyes and %s hair" %(my_eyes, my_hair)
+print "His theeth are usually %s depending on the coffee" %my_theeth
+#hard line
+print "If you add %d, %d, %d you get %d" %(my_age, my_height, my_weight, my_age+my_height+my_weight)
