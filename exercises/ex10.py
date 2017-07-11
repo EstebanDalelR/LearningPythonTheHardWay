71438ba7378df368e018b7724070b03501d42748
estebandalelr <estebandalelr@gmail.com>
2017-07-11 16:45:23 -0500

    finish exercise 10

---
+++ exercises/ex10.py
@@ -0,0 +1,13 @@
+tabby_cat = "\tI'm tabbed in"
+persian_cat = "I'm split \non a line"
+backslash_cat = "I \\ am a \\ cat"
+fat_cat = """
+I'll do a list:
+\t*pizza
+\t*lasagna
+\t*burguers \n\t*soda
+"""
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
