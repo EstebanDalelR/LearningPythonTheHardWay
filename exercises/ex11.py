34d8f32db3a9e4a6f34f15cdfeba8c5340658658
estebandalelr <estebandalelr@gmail.com>
2017-07-13 10:40:19 -0500

    finish exercise 11

---
+++ exercises/ex11.py
@@ -0,0 +1,8 @@
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "What's your weight?",
+weight = raw_input()
+
+print "So you are %r, your height is %r and you weight %r" % (age, height, weight)
