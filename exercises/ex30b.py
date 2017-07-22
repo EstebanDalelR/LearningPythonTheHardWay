8487b53e7afc92b52f1fe64d142348576d14c470
estebandalelr <estebandalelr@gmail.com>
2017-07-22 10:32:01 -0500

    finished exercise 30b

---
+++ exercises/ex30b.py
@@ -0,0 +1,17 @@
+people = 30
+cars = 40
+buses =95
+
+if cars > people:
+    print "we should take the cars"
+elif cars < people:
+    print "we should NOT take the cars"
+else:
+    print "what the f*ck"
+
+if buses > people:
+    print "unnecesary amount of buses"
+elif people > buses:
+    print "take the bus"
+else:
+    print "wtf"
