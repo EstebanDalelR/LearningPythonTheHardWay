d61cf4f5d279b609993f24eec4a1bd26ada9b9a9
estebandalelr <estebandalelr@gmail.com>
2017-07-22 10:11:27 -0500

    finished exercise 29

---
+++ exercises/ex29.py
@@ -0,0 +1,28 @@
+people = 20
+cats = 30
+dogs = 15
+
+if people < cats:
+    print "so many cats omg"
+
+if people > cats:
+    print "we need more felines"
+
+if people < dogs:
+    print "woof woof"
+
+if people > dogs:
+    print "hello"
+
+dogs += 5
+
+if people >= dogs:
+    print "people are greater or equal to dogs"
+
+if people <= dogs:
+    print "is it fewer or less than dogs?"
+
+if people == dogs:
+    print "people are the same as dogs, give them rights"
+
+     
